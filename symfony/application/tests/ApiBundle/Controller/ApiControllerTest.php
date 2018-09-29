bc4bf88158ec590402cbcc2cf99c5dab5b38af39
rsilveira65 <rsilveiracc@gmail.com>
2018-09-29 12:40:18 -0300

    Adding testCases for ApiController unit tests.

--- symfony/application/tests/ApiBundle/Controller/ApiControllerTest.php
+++ symfony/application/tests/ApiBundle/Controller/ApiControllerTest.php
@@ -15,6 +15,13 @@ class ApiControllerTest extends \PHPUnit_Framework_TestCase
     /** @var  \GuzzleHttp\Client $client */
     private $client;
 
+    private $testCases = [
+        ['origin' => 1, 'destination' => 2, 'plan' => 1, 'time' => 20, 'planRateCost'=> 0, 'rateCost' => 38],
+        ['origin' => 1, 'destination' => 3, 'plan' => 2, 'time' => 80, 'planRateCost'=> 37, 'rateCost' => 136],
+        ['origin' => 4, 'destination' => 1, 'plan' => 3, 'time' => 200, 'planRateCost'=> 167, 'rateCost' => 380],
+        ['origin' => 4, 'destination' => 3, 'plan' => 1, 'time' => 100, 'planRateCost'=> null, 'rateCost' => null],
+    ];
+
     public function setUp()
     {
         $this->client = new \GuzzleHttp\Client(['base_uri' => 'http://nginx/']);
@@ -25,21 +32,34 @@ class ApiControllerTest extends \PHPUnit_Framework_TestCase
      */
     public function testCalculate()
     {
-        $response = $this->client->post('/api/calculate/1/3/2/80');
+        foreach ($this->testCases as $testCase) {
+            $response = $this->client->post(
+                sprintf(
+                    '/api/calculate/%s/%s/%s/%s',
+                    $testCase['origin'],
+                    $testCase['destination'],
+                    $testCase['plan'],
+                    $testCase['time']
+                )
+            );
 
-        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+            $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
 
-        $data = json_decode($response->getBody(), true);
+            $data = json_decode($response->getBody(), true);
 
-        $this->assertArrayHasKey('id', $data);
-        $this->assertArrayHasKey('origin', $data);
-        $this->assertArrayHasKey('destination', $data);
-        $this->assertArrayHasKey('rate', $data);
-        $this->assertArrayHasKey('time', $data);
-        $this->assertArrayHasKey('rateCost', $data);
-        $this->assertArrayHasKey('planRateCost', $data);
-        $this->assertArrayHasKey('id', $data['plan']);
-        $this->assertArrayHasKey('name', $data['plan']);
-        $this->assertArrayHasKey('time', $data['plan']);
+            $this->assertEquals($testCase['rateCost'], $data['rateCost']);
+            $this->assertEquals($testCase['planRateCost'], $data['planRateCost']);
+
+            $this->assertArrayHasKey('id', $data);
+            $this->assertArrayHasKey('origin', $data);
+            $this->assertArrayHasKey('destination', $data);
+            $this->assertArrayHasKey('rate', $data);
+            $this->assertArrayHasKey('time', $data);
+            $this->assertArrayHasKey('rateCost', $data);
+            $this->assertArrayHasKey('planRateCost', $data);
+            $this->assertArrayHasKey('id', $data['plan']);
+            $this->assertArrayHasKey('name', $data['plan']);
+            $this->assertArrayHasKey('time', $data['plan']);
+        }
     }
 }
